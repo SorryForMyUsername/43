5731d2bd1a0b0aa439bf718dd9741e4c50eab6cf
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-22 11:04:12 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_3
+{
+    internal struct Book
+    {
+        public string title;
+        public string author;
+        public int publicationYear;
+        int pages;
+
+        public Book(string title, string author, int publicationYear, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.publicationYear = publicationYear;
+            this.pages = pages;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Название: {title}\n" +
+                $"Ф.И.О автора: {author}\n" +
+                $"Год издания: {publicationYear}\n" +
+                $"Количество страниц: {pages}");
+        }
+
+        public static Book Input()
+        {
+            Console.Write("Название: ");
+            string title = Console.ReadLine();
+            Console.Write("Ф.И.О. автора: ");
+            string author = Console.ReadLine();
+            Console.Write("Год издания: ");
+            int publicationYear = int.Parse(Console.ReadLine());
+            Console.Write("Кол-во страниц: ");
+            int pages = int.Parse(Console.ReadLine());
+
+            return new Book(title, author, publicationYear, pages);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие: \n" +
+                    "1. Создать книгу\n" +
+                    "2. Вывести информацию о книгах\n" +
+                    "3. Выход");
+
+                int choice = Console.ReadKey(true).KeyChar - '0';
+
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case 1: books.Add(Book.Input()); break;
+                    case 2: OutputBooks(books); break;
+                    case 3: return;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void OutputBooks(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                book.Output();
+                Console.WriteLine();
+            }
+        }
+    }
+}
