469274d411ea52b0575b604632e020096e87660b
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-22 10:48:52 +0300

    Add files via upload

---
+++ Notebook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_2
+{
+    internal struct Notebook
+    {
+        public string model;
+        public string manufacturer;
+        public int price;
+
+        public Notebook(string model, string manufacturer, int price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Модель: {model}\n" +
+                $"Производитель: {manufacturer}\n" +
+                $"Цена: {price}");
+        }
+    }
+}
