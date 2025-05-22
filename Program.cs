322744eaea3610c8180e6e1f97161095087b57fd
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-22 10:42:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+
+            Console.WriteLine($"myStruct: {myStruct.change}\nmyClass: {myClass.change}");
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+    }
+}
